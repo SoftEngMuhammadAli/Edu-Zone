f5904989b54e8713e5e073ae77e2703b8d28bbb3
Muhammad Ali <softeng.aliijaz@gmail.com>
2025-06-14 16:17:24 +0500

    Removed nested git repo and added project folders

---
+++ eduzone-lms-app/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen gap-10">
+      <h2 className="text-2xl">Welcome to Our Learning Platform</h2>
+      <p className="w-1/2 text-center">
+        EduZone is a comprehensive Learning Management System (LMS) designed to
+        provide a seamless learning experience for students and educators. Our
+        platform offers a range of features, including course creation and
+        management, lesson planning and delivery, assignment submission and
+        grading, and user dashboard for tracking progress. With EduZone,
+        educators can easily create and manage courses, assignments, and grades,
+        while students can access course materials, submit assignments, and
+        track their progress. Our goal is to provide a user-friendly and
+        efficient learning platform that meets the needs of both educators and
+        students.
+      </p>
+    </div>
+  );
+}
+
+export default App;
