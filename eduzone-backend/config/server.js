aafae5110288d92b977d2afb06a4ae3c5ce056b3
Muhammad Ali <softeng.aliijaz@gmail.com>
2025-06-17 19:16:17 +0500

    updated for deployment

--- eduzone-backend/config/server.js
+++ eduzone-backend/config/server.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 async function connectToDatabase(databaseUrl) {
   try {
-    await mongoose.connect(databaseUrl);
-    console.log("Eduzone is connected to database successfully!");
+    await mongoose.connect(databaseUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Eduzone is connected to the database successfully!");
   } catch (e) {
-    console.error("Error while connecting to database", e);
+    console.error("❌ Error while connecting to database:", e.message);
     process.exit(1);
   }
 }
