8df67633e85b746dce2996f4c449c42d9eea46d7
Muhammad Ali <softeng.aliijaz@gmail.com>
2025-06-24 22:04:31 +0500

    refactor: reorganize imports in Routes.jsx and remove index.jsx file

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,24 +1,22 @@
+import React from "react";
 import {
   Route,
   BrowserRouter as Router,
   Routes,
   useLocation,
 } from "react-router-dom";
-import {
-  CourseDetails,
-  Help,
-  Home,
-  LoginPage,
-  NotFound,
-  ReadAllBlogs,
-  SeeAllCoursesList,
-  SignUpPage,
-} from "../pages";
-import React from "react";
 import AboutUs from "../components/about/AboutUs";
 import ContactUs from "../components/contact/ContactUs";
 import CourseSuccess from "../components/courses/CourseSuccess";
 import HeaderNav from "../components/home/HeaderNav";
+import CourseDetail from "../pages/courses/CourseDetails";
+import Help from "../pages/help/Help";
+import Home from "../pages/home/Home";
+import LoginPage from "../pages/auth/LoginPage";
+import SignUpPage from "../pages/auth/SignUpPage";
+import SeeAllCoursesList from "../pages/courses/AllCourses";
+import ReadAllBlogs from "../pages/blogs/AllBlogs";
+import NotFound from "../pages/home/NotFound";
 
 const AppRoutes = () => {
   return (
@@ -57,7 +55,7 @@ const MainLayout = () => {
         <Route path="/allblogs" element={<ReadAllBlogs />} />
 
         {/* Courses Paths */}
-        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/course/:id" element={<CourseDetail />} />
         <Route path="/coursesuccess" element={<CourseSuccess />} />
       </Routes>
     </>
